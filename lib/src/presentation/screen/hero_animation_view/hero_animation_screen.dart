82e8e8650f7457045a6438f141d2656d613625f6
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-11-05 00:32:45 +0530

    creating product list screen

--- lib/src/presentation/screen/hero_animation_view/hero_animation_screen.dart
+++ lib/src/presentation/screen/hero_animation_view/hero_animation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:gallery/main.dart';
 import 'package:gallery/src/data/models/product_model.dart';
 import 'package:gallery/src/presentation/presentation.dart';
@@ -22,69 +23,83 @@ class _HeroAnimationScreenState extends BaseStateWrapper<HeroAnimationScreen> {
       appBar: const MAppBar(
         title: Text('Product List'),
       ),
-      body: ListView.builder(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          itemCount: productList.length,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            return Container(
-              width: context.screenWidth * .8,
-              padding: const EdgeInsets.only(
-                  bottom: 10, top: 10, left: 15, right: 15),
-              margin: const EdgeInsets.only(bottom: 15),
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                shadows: const [
-                  BoxShadow(
-                      color: Color(0X3F000000),
-                      blurRadius: 15,
-                      offset: Offset(5, 5),
-                      spreadRadius: 0)
-                ],
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    height: 60,
-                    width: 60,
-                    decoration: BoxDecoration(
-                        color: MColorScheme.greyColorPalette[200],
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(
-                            color: MColorScheme.backgroundColor, width: 1)),
-                    child: Center(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(5),
-                        child: Image.network(
-                          productList[index].imageUrl,
-                          height: 60,
-                          width: 60,
-                        ),
+      body: AnimationLimiter(
+        child: ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            itemCount: productList.length,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return AnimationConfiguration.staggeredList(
+                position: index,
+                duration: const Duration(milliseconds: 800),
+                child: SlideAnimation(
+                  verticalOffset: -50.0,
+                  horizontalOffset: -50.0,
+                  child: FadeInAnimation(
+                    child: Container(
+                      width: context.screenWidth * .8,
+                      padding: const EdgeInsets.only(
+                          bottom: 10, top: 10, left: 15, right: 15),
+                      margin: const EdgeInsets.only(bottom: 15),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        shadows: const [
+                          BoxShadow(
+                              color: Color(0X3F000000),
+                              blurRadius: 15,
+                              offset: Offset(5, 5),
+                              spreadRadius: 0)
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                color: MColorScheme.greyColorPalette[200],
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(
+                                    color: MColorScheme.backgroundColor,
+                                    width: 1)),
+                            child: Center(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Image.network(
+                                  productList[index].imageUrl,
+                                  height: 60,
+                                  width: 60,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 15),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(productList[index].productName,
+                                  style: const TextStyle(
+                                      color: darkBlue,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(height: 5),
+                              Text(
+                                "\$${productList[index].price.toString()}",
+                                style: const TextStyle(
+                                  color: darkBlue,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  const SizedBox(width: 15),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(productList[index].productName,
-                          style: const TextStyle(
-                              color: darkBlue, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 5),
-                      Text(
-                        "\$${productList[index].price.toString()}",
-                        style: const TextStyle(
-                          color: darkBlue,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          }),
+                ),
+              );
+            }),
+      ),
     );
   }
 
